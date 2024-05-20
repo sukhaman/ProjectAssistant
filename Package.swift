1b417a29795d82fc9371551188153b4ca6112c3f
Sukhaman Singh <sukhamandev@gmail.com>
2024-05-20 13:50:38 -0400

    Update Package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ProjectAssistant", path: "./Sources/ProjectAssistant/ProjectAssistantFramework.xcframework"
+        .binaryTarget(name: "ProjectAssistant", path: "Sources/ProjectAssistant/ProjectAssistantFramework.xcframework"
         ),
         .testTarget(
             name: "ProjectAssistantTests",
